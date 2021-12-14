14a0a2719ea5e523f99f5921f7a631fc7a693455
Isaac Manjarrez Leyva <saacmanjarrez@gmail.com>
2021-12-13 20:20:58 -0600

    Update: note backend

--- note/src/main/java/backend/note/NoteApplication.java
+++ note/src/main/java/backend/note/NoteApplication.java
@@ -1,4 +1,4 @@
-package note.note;
+package backend.note;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- note/src/test/java/backend/note/NoteApplicationTests.java
+++ note/src/test/java/backend/note/NoteApplicationTests.java
@@ -1,4 +1,4 @@
-package note.note;
+package backend.note;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
