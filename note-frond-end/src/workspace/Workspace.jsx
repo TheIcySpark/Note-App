231382aab5e9bc02544f9519915eff383d2408c6
Isaac Manjarrez Leyva <saacmanjarrez@gmail.com>
2021-12-25 22:57:12 -0600

    Add: create note button, improve UI

--- note-frond-end/src/workspace/Workspace.jsx
+++ note-frond-end/src/workspace/Workspace.jsx
@@ -7,13 +7,21 @@ import LogoutButon from "../login/LogoutButton";
 export default class Workspace extends React.Component{
     constructor(props){
         super(props);
-        this.state = {text: ""}
+        this.state = {text: '', notes: []}
 
         this.handleTextChange = this.handleTextChange.bind(this);
+        this.handleNewNote = this.handleNewNote.bind(this);
     }
 
     handleTextChange(value){
         this.setState({text: value});
+    }
+
+    handleNewNote(){
+        let updatedNotes = [...this.state.notes];
+        updatedNotes.push(1);
+        this.setState({notes: updatedNotes})
+        console.log("New Note");
     }
     
     render() {
@@ -21,88 +29,25 @@ export default class Workspace extends React.Component{
             overflowY: "scroll",
             height: '95vh'
         }
+        const allNotes = this.state.notes.map((note) =>
+            <button key={note} type="button" className="btn btn-dark w-100">
+                <i className="fas fa-sticky-note"></i>
+            </button>
+        );
+        console.log("render");
         return (
             <div>
                 <div className="container-fluid">
                     <br></br>
                     <div className="row">
-                        <div className="col-md-1" >
+                        <div className="col-md-1">
+                            <button type="button" className="btn btn-dark w-100" onClick={this.handleNewNote}>
+                                <i className="fas fa-plus-circle"></i>
+                            </button>
                             <LogoutButon handleLogout = {this.props.handleLogout}/>
                         </div>
                         <div className="col-md-2" style={scrollStyle}>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-                            <button>ir a la nota</button>
-
+                            {allNotes}
                         </div>
                         <div className="col-md-9" style={scrollStyle} >
                             <ReactQuill
