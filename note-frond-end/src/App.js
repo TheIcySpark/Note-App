4f0235ef70bc5e645f8ee30c69aad042515cf46d
javis83 <95990137+javis83@users.noreply.github.com>
2021-12-28 21:53:34 -0600

    header

--- note-frond-end/src/App.js
+++ note-frond-end/src/App.js
@@ -2,10 +2,21 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import LoginControl from './login/LoginControl';
+import logo from './logo2.jpg';
 
 function App() {
     return (
-        <LoginControl/>
+        <div className="App-note">
+            <nav className="nav navbar navbar-dark bg-dark">
+                <h1 className="titulo">
+                    Note App
+                    <img src={logo} className="App-logo"></img>
+                </h1>
+                
+            </nav>
+            <LoginControl/>
+        </div>
+        
     );
 }
     
